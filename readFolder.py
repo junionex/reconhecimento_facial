a4d78f16e44a2e8453166eade030f524d760b94a
junionex <junionex.junior@gmail.com>
2025-07-31 04:32:47 -0300

    Foi aplicado uma função para multi processamento, usando mais CPU

--- readFolder.py
+++ readFolder.py
@@ -1,13 +1,32 @@
 from pathlib import Path
 from picture import buscarRostos
+from progressBar import progresso_de_carregamento
+from multiprocessing import Pool
+
 
 # Caminho da pasta
 caminho_pasta = Path("./img")
 
 # Listar os arquivos
 arquivos = [arquivo.name for arquivo in caminho_pasta.iterdir() if arquivo.is_file()]
-print("total de img: " + str((len(arquivos))))
-for arquivo in arquivos:
-    print("Buscando os Rostos do arquivo: " + arquivo)
-    buscarRostos(arquivo)
+total_de_arquivos = len(arquivos)
+#print("total de img: " + str(total_de_arquivos))
+#for i, arquivo in enumerate(arquivos):
+#    print("\rBuscando os Rostos do arquivo: " + arquivo)
+#    progresso_de_carregamento(i + 1, total_de_arquivos)
+#    buscarRostos(arquivo)
 #buscarRostos("familia.jpg")
+
+def processa_arquivo(args):
+    i, arquivo = args
+    print("\rBuscando os Rostos do arquivo: " + arquivo)
+    progresso_de_carregamento(i + 1, total_de_arquivos)
+    buscarRostos(arquivo)
+
+if __name__ == "__main__":
+    # Usar multiprocessing para processar os arquivos em paralelo
+    # Isso pode acelerar o processo se houver muitos arquivos
+    # Pool(processes=4)  # Usa 4 processos Ideal diminuir se n quiser explodir o pc
+    with Pool(processes=6) as pool:
+        pool.map(processa_arquivo, list(enumerate(arquivos)))
+    
